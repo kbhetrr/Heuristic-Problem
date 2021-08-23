d6a0e6a904896f80205f7fff950f4ce09cef8be0
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-23 14:59:24 +0900

    814 프로젝트 추가

---
+++ 814/cmake-build-debug/CMakeFiles/814.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "814.exe"
+  "814.exe.manifest"
+  "814.pdb"
+  "CMakeFiles/814.dir/main.cpp.obj"
+  "lib814.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/814.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
